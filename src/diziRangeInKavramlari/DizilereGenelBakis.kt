3c9f9f204b03397b3265b82bd2843e9f186b739c
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-03 20:52:22 +0300

    dizilere genel bir bakış attık

---
+++ src/diziRangeInKavramlari/DizilereGenelBakis.kt
@@ -0,0 +1,24 @@
+package diziRangeInKavramlari
+
+fun main(args: Array<String>) {
+
+
+    var sayi:Int = 50
+    var sayilar : Array<Int> = arrayOf(1,2,3,4,5,6)
+
+    println(sayilar[0])
+    println(sayilar.get(5))
+    println("Toplam eleman sayısı " + sayilar.size)
+
+    var kelimeler : Array<String> = arrayOf("emre","ali","hasan")
+
+    var degiskenlerim : Array<Any> = arrayOf(1,"Emre", true)
+
+    println(degiskenlerim.get(1))
+
+    var gunler = arrayOf("pazartesi" , "salı", "carşamba")
+
+
+}
+
+
