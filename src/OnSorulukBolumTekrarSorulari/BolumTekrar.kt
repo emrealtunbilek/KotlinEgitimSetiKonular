288a2cef401a40a16cde09a275afc4928f885fe9
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-05 00:19:44 +0300

    1 2 ve 3. soru çözümleri yapıldı

---
+++ src/OnSorulukBolumTekrarSorulari/BolumTekrar.kt
@@ -0,0 +1,63 @@
+package OnSorulukBolumTekrarSorulari
+
+fun main(args: Array<String>) {
+
+    /*
+   //ÇÖZÜM 1
+    println("Birinci sayıyı giriniz")
+    var sayi1 : Int = readLine()!!.toInt()
+
+    println("İkinci sayıyı giriniz")
+    var sayi2 : Int = readLine()!!.toInt()
+
+    println("Üçüncü sayıyı giriniz")
+    var sayi3 : Int = readLine()!!.toInt()
+
+    var ortalama:Double = ((sayi1+sayi2+sayi3).toDouble() / 3)
+
+    println("Girdiğiniz sayıların ortalaması : " + ortalama)
+*/
+
+    /*
+    //ÇÖZÜM 2
+
+    println("Birinci kenarı giriniz : ")
+    var birinciKenar : Int = readLine()!!.toInt()
+
+    println("İkinci kenarı giriniz : ")
+    var ikinciKenar : Int = readLine()!!.toInt()
+
+    println("Üçüncü kenarı giriniz : ")
+    var ucuncuKenar : Int = readLine()!!.toInt()
+
+    if ( (birinciKenar == ikinciKenar) && (birinciKenar == ucuncuKenar)){
+
+        println("Girilen üçgen eşkenar bir üçgendir.")
+
+    }else if ((birinciKenar != ikinciKenar) && (birinciKenar != ucuncuKenar) && (ikinciKenar != ucuncuKenar)){
+        println("Girilen üçgen çeşitkenardır")
+
+    }else {
+        println("Girğilen üçgen ikizkenardır")
+    }*/
+
+    /*
+    //ÇÖZÜM 3
+    println("Vize notunuz : ")
+    var vize:Double = readLine()!!.toDouble()
+    println("Final notunuz : ")
+    var finalNotu = readLine()!!.toDouble()
+
+    var ortalama = ((vize*40)/100) + ((finalNotu*60)/100)
+
+    if(ortalama >= 50){
+
+        println("Tebrikler dersi geçtiniz Ortalamanız : " + ortalama)
+    }else {
+        println("Yaz okulunun yolu göründü")
+    }
+    */
+
+
+
+}
