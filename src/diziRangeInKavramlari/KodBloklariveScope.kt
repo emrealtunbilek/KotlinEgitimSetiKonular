b70fb67208d0b4d0c90d66da5a1132f232a81274
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-04 13:41:41 +0300

    kod blokları ve scope anlatıldı

---
+++ src/diziRangeInKavramlari/KodBloklariveScope.kt
@@ -0,0 +1,36 @@
+package diziRangeInKavramlari
+
+var sayi : Int = 10
+
+fun main(args: Array<String>) {
+    run {
+
+    var seviye1 : Int = 1
+        run {
+
+            println(seviye1)
+
+        }
+        var seviye2:Int = 2
+        {
+
+            println(seviye1)
+            println(seviye2)
+
+            var seviye3:Int = 3
+            println(seviye3)
+            run {
+
+
+
+            }
+
+        }
+
+
+
+
+    }
+
+}
+
