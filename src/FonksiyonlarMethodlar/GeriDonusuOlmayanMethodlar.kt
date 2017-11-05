c03e324fa6721ce57c71c942f990395d31f301b7
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-05 20:34:20 +0300

    geri dönüş tipi olmayan methodlar anlatıldı parametre alıp almadıkları da incelendi

---
+++ src/FonksiyonlarMethodlar/GeriDonusuOlmayanMethodlar.kt
@@ -0,0 +1,34 @@
+package FonksiyonlarMethodlar
+
+fun main(args: Array<String>) {
+
+   // isimYazdir()
+    ismiParametreIleYazdir("Emre Altunbilek", 29)
+
+    menuGoster()
+
+
+
+}
+
+fun ismiParametreIleYazdir(isim : String , yas:Int) {
+
+    println(isim)
+    println("Yaş : " + yas)
+
+}
+
+fun isimYazdir(){
+    println("Emre Altunbilek")
+    println("29")
+}
+
+fun menuGoster(){
+
+    println("MENU GOSTER")
+    println("1 - Iki sayının toplamını bul")
+    println("2 - Iki sayının farıkını bul")
+    println("3 - Iki sayının çarpımı bul")
+
+}
+
