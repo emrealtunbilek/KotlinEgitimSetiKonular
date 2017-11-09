2fdb4b2e41bad72daa623c007cd74e54d6e4d2f0
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-10 01:57:56 +0300

    polimorfizm olmasaydı

---
+++ src/Kalitim/PolimorfizmOlmasaydi.kt
@@ -0,0 +1,98 @@
+package Kalitim
+
+open class Calisan {
+
+     open var pozisyon:String="Çalışan"
+
+     open fun calis(){
+
+        println("$pozisyon çalışmaya başladı")
+    }
+
+}
+
+class Mudur : Calisan(){
+
+    override var pozisyon: String = "Müdür"
+
+     override fun calis() {
+         println("$pozisyon çalışmaya başladı")
+    }
+
+
+}
+
+class Programci : Calisan(){
+
+    override var pozisyon: String = "Programcı"
+
+    override fun calis() {
+        println("$pozisyon çalışmaya başladı")
+    }
+}
+
+class Pazarlamaci : Calisan(){
+
+    override var pozisyon: String = "Pazarlamacı"
+
+    override fun calis() {
+        println("$pozisyon çalışmaya başladı")
+    }
+}
+
+fun main(args: Array<String>) {
+
+   /* var calisanlar=Array<Any>(4){Any()}
+
+    calisanlar[0]=Calisan() //yukarı çevrim upcasting
+    calisanlar[1]=Mudur()
+    calisanlar[2]=Programci()
+    calisanlar[3]=Pazarlamaci()*/
+
+    var calisanlar=Array<Calisan>(4){Calisan()}
+
+    calisanlar[0]=Calisan() //yukarı çevrim upcasting
+    calisanlar[1]=Mudur()
+    calisanlar[2]=Programci()
+    calisanlar[3]=Pazarlamaci()
+
+    //mesaiyeBasla(calisanlar)
+    mesaiyeBaslaPolimorfizm(calisanlar)
+
+
+}
+
+fun mesaiyeBasla(calisanlar: Array<Any>) {
+
+    for (oankiCalisan in calisanlar){
+
+        if (oankiCalisan is Calisan){
+           var calisan:Calisan=oankiCalisan // aşağıya çevrim
+            calisan.calis()
+        }else if(oankiCalisan is Mudur){
+
+            var mudur:Mudur=oankiCalisan
+            mudur.calis()
+
+        }else if(oankiCalisan is Programci){
+            var programci:Programci= oankiCalisan
+            programci.calis()
+        }else if(oankiCalisan is Pazarlamaci) {
+            var pazarlamaci:Pazarlamaci=oankiCalisan
+            pazarlamaci.calis()
+        }
+
+
+    }
+
+
+}
+
+fun mesaiyeBaslaPolimorfizm(calisanlar: Array<Calisan>){
+
+    for (oankiCalisan in calisanlar){
+
+        oankiCalisan.calis()
+    }
+
+}
