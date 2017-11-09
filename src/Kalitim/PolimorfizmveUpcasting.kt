b9d8a65486c1b175adcb1c755dd1da56d4fc3c23
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-09 23:22:42 +0300

    polimorfizm ve upcasting işlendi

---
+++ src/Kalitim/PolimorfizmveUpcasting.kt
@@ -0,0 +1,42 @@
+package Kalitim
+
+open class Asker {
+
+    open fun selamVer(){
+        println("Asker selam verdi")
+    }
+}
+
+class Er : Asker(){
+    override fun selamVer() {
+        println("Er selam verdi")
+    }
+
+}
+
+class Yuzbasi : Asker(){
+    override fun selamVer() {
+        println("Yüzbaşı selam verdi")
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+    var asker=Asker()
+    var er=Er()
+    var yuzbasi=Yuzbasi()
+
+
+
+    hazirOl(asker)
+    hazirOl(er)
+    hazirOl(yuzbasi)
+
+}
+
+fun hazirOl(asker:Asker){
+
+    asker.selamVer()
+
+}
