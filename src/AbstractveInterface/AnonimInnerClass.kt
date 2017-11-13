68dbcc01e362c97becd182207864f9fcabff85dc
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-14 00:39:31 +0300

    anonim inner classlar anlatıldı

---
+++ src/AbstractveInterface/AnonimInnerClass.kt
@@ -0,0 +1,28 @@
+package AbstractveInterface
+
+interface Okuyabilen{
+    fun oku()
+}
+
+class Tarayici : Okuyabilen{
+
+    override fun oku() {
+        println("Tarayici sınıfından oku methodu tetiklendi")
+    }
+}
+
+fun main(args: Array<String>) {
+
+    var tarayici=Tarayici()
+    tarayici.oku()
+
+    var isimsizSinif= object : Okuyabilen{
+
+        override fun oku() {
+            println("isimsiz sınıfındaki oku methodu tetiklendi")
+        }
+
+    }
+    isimsizSinif.oku()
+
+}
