558fab70e40befab78cf2af6249d8d221ed88b24
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-15 05:29:56 +0300

    değiştirilemez mapler ve mapOf anlatıldı

---
+++ src/Koleksiyonlar/DegistirilemeyenMap.kt
@@ -0,0 +1,24 @@
+package Koleksiyonlar
+
+fun main(args: Array<String>) {
+
+    //mapof ile degistirlemeyen map
+    var map = mapOf<Int, String>(1 to "emre", 2 to "hasan", 3 to "ali")
+
+    println("keyi 2 olan değer " + map.get(2))
+
+    for(str in map){
+
+        println(str.key)
+        println(str.value)
+    }
+
+    var aranacakIfade= readLine()!!
+    if(map.containsValue(aranacakIfade)){
+
+        println("$aranacakIfade map de var")
+
+    }else {
+        println("$aranacakIfade map de yok")
+    }
+}
