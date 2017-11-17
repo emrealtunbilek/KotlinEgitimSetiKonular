41235f7c550fbf0d144067095fd06a229f6d2171
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-17 20:18:17 +0300

    set küme yapısı anlatıldı

---
+++ src/Koleksiyonlar/SetKumeYapisi.kt
@@ -0,0 +1,28 @@
+package Koleksiyonlar
+
+//setof, mutablesetof
+
+fun main(args: Array<String>) {
+
+
+    var degistirilemeyenSet= setOf("emre", "hasan", 5,9,"emre")
+
+    for (oankiDeger in degistirilemeyenSet)
+        println(oankiDeger)
+
+
+    var degistirilebilenSet= mutableSetOf<Int>()
+
+    degistirilebilenSet.add(9)
+    degistirilebilenSet.add(6)
+    degistirilebilenSet.add(1)
+    degistirilebilenSet.add(6)
+    degistirilebilenSet.add(9)
+
+    println("Size" + degistirilebilenSet.size)
+    println(degistirilebilenSet)
+
+    for (oankiSayi in degistirilebilenSet)
+        println(oankiSayi)
+
+}
