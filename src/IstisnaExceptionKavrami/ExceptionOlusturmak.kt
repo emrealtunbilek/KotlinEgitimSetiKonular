2251af762555b57b4b3e7c23f829fbe310e635a7
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-18 11:45:52 +0300

    finally blogu ve kendi hatamızı olusturma konuları anlatıldı

---
+++ src/IstisnaExceptionKavrami/ExceptionOlusturmak.kt
@@ -0,0 +1,44 @@
+package IstisnaExceptionKavrami
+
+class Person{
+
+    var yas : Int = 0
+    get() = field
+    set(value) {
+
+        if(value < 0){
+
+            val istisna=IllegalArgumentException("Personel yaşı negatif olamaz")
+            throw istisna
+
+        }else {
+
+            field = value
+        }
+
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+    var p1=Person()
+    println(p1.yas)
+    try {
+        p1.yas=-9
+        println(p1.yas)
+
+    }catch (istisna:Exception){
+
+        println("hata 1 : " + istisna.message)
+        println("hata 2 : " + istisna.toString())
+
+    }finally {
+        println("Program bitti")
+    }
+
+
+
+
+}
+
