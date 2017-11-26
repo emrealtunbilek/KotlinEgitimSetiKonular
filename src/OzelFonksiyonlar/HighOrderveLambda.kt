33e6aa6afabc5f187b8b2ba1ea6753446ca6c68e
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-26 20:55:35 +0300

    high order func ve lambda 2. bölüm anlatıldı

---
+++ src/OzelFonksiyonlar/HighOrderveLambda.kt
@@ -0,0 +1,67 @@
+package OzelFonksiyonlar
+
+import FonksiyonlarMethodlar.topla
+
+fun main(args: Array<String>) {
+
+    var nesne=Topla()
+    nesne.ikiSayiyiTopla(3,5)  // normal yol
+
+    /*var interfaceNesne=Test()
+    nesne.ikiSayiyiTopla(6,10, interfaceNesne)
+    //anonim inner sınıf kullanmasaydık
+    */
+
+    nesne.ikiSayiyiTopla(6,10, object : ToplamGosteren{
+        override fun toplamGosteren(toplam: Int) {
+            println(toplam)             //interface kullanarak
+        }
+
+    })
+
+    var isim:String="emre"
+
+   var lambdaExpression = { sayi:Int -> println(sayi)}  //lambda expression,, isimsiz fonksiyon, sayi:parametre -> fonksiyon gövdesi gelir
+
+    nesne.ikiSayiyiTopla(9, 15, lambdaExpression)
+
+    nesne.ikiSayiyiTopla(9, 15, {sayi:Int -> println(sayi)})
+
+}
+
+class Topla {
+
+
+    fun ikiSayiyiTopla(a:Int, b:Int){
+        val toplam= a + b
+        println(toplam)
+    }
+
+    fun ikiSayiyiTopla(a:Int, b:Int, action:ToplamGosteren){
+        val toplam= a + b
+        action.toplamGosteren(toplam)
+    }
+
+    fun ikiSayiyiTopla(a:Int, b:Int, action:(Int) -> Unit){
+
+        val toplam=a+b
+        action(toplam)
+
+    }
+
+
+}
+
+interface ToplamGosteren {
+
+    fun toplamGosteren(toplam:Int)
+
+}
+
+class Test:ToplamGosteren{
+    override fun toplamGosteren(toplam: Int) {
+        println(toplam)
+    }
+
+
+}
