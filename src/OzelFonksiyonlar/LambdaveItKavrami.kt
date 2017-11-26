4e923a3dcd233cbba263e47fce4063d38399a81d
emre altunbilek <emrealtunbilek@gmail.com>
2017-11-26 22:07:31 +0300

    tek parametreli lambda fonksiyonlardaki it kavramı anlatıldı

---
+++ src/OzelFonksiyonlar/LambdaveItKavrami.kt
@@ -0,0 +1,39 @@
+package OzelFonksiyonlar
+
+class TestLambda{
+
+    fun ikiSayiyiTopla(sayi1:Int, sayi2:Int, action:(Int, Int)-> Int){
+
+        val toplam= action(sayi1,sayi2)
+        println(toplam)
+
+    }
+
+    fun terstenYaz(yazi:String, myFunc : (String) -> String){
+
+        var testenYazi = myFunc(yazi)
+        println(testenYazi)
+
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+    var nesne=TestLambda()
+
+    var lambdaFonksiyon = {a:Int, b:Int -> a+b}
+
+    nesne.ikiSayiyiTopla(9,12, lambdaFonksiyon)
+    nesne.ikiSayiyiTopla(9,12, {a:Int, b:Int -> a+b})
+    nesne.ikiSayiyiTopla(9,12) {a:Int, b:Int -> a+b}
+
+    var lambdaFonkString = {yazi:String -> yazi.reversed()}
+    nesne.terstenYaz("merhaba" , lambdaFonkString)
+    nesne.terstenYaz("merhaba" , {yazi:String -> yazi.reversed()})
+    nesne.terstenYaz("merhaba") {yazi:String -> yazi.reversed()}
+    nesne.terstenYaz("merhaba") {it.reversed()} // tek parametreli fonksiyonlar için
+
+
+
+}
