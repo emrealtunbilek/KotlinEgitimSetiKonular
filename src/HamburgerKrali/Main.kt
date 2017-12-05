7a09c33f7cf347cc59864d4fd033221d84240118
emre altunbilek <emrealtunbilek@gmail.com>
2017-12-05 21:43:17 +0300

    hamburger krali uygulaması yapılıyor

---
+++ src/HamburgerKrali/Main.kt
@@ -0,0 +1,19 @@
+package HamburgerKrali
+
+fun main(args: Array<String>) {
+    /* Burger Kralı Satış Uygulaması
+       Bu uygulamanın amacı işletmenin hamburger satış işlemlerini gerçekleştirmesini sağlamaktır.
+       Uygulama hamburger bilgilerini ve satış bilgilerini tutacaktır.
+       Mağazada 3 çeşit hamburger bulunmaktadır.
+
+        1)Sade Hamburger : Ekmek türü ve İçindeki Et çeşidi ve 4 tane extra bilgisi alabilir.
+          Müşterinin istediği extra şeyler (sos, domates, peynir gibi) toplam fiyata dahil edilmelidir.
+
+        2)Sağlıklı Burger : Bu burgerın ekmek türü kepeklidir. 6 taneye kadar ekstra ürün daha içerebilir.
+
+        3)Baba Burger : Bu burger da patates kızartması ve içecek kendinden tanımlıdır. Ekstra ürün içeremez.
+
+    Fiyatları kendiniz beliryebilirsiniz.
+    Önemli olan bu yapıyı nesneye yönelimli programlama öğelerini kullanarak gerçekleştirebilmeniz..
+    */
+}
